630f1717278e32d0149318ddfc0d6614a043534b
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-09-23 13:17:11 +0200

    searchpage up

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -10,17 +10,19 @@ export default function Page({ searchParams }: PageProps) {
   const { image, title, author, source, published_at, description }: Article = searchParams;
 
   return (
-    <article className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-      {image && <img src={image} alt={title} className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md" />}
-      <div className="px-10">
-        <h1 className="headerTitle px-0 no-underline pb-2">{title}</h1>
-        <div>
-          <h2>{author}</h2>
-          <h2>Source: {source}</h2>
-          <p className="pl-4">{published_at}</p>
+    <main>
+      <article className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
+        {image && <img src={image} alt={title} className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md" />}
+        <div className="px-10">
+          <h1 className="headerTitle px-0 no-underline pb-2">{title}</h1>
+          <div>
+            <h2>{author}</h2>
+            <h2>Source: {source}</h2>
+            <p className="pl-4">{published_at}</p>
+          </div>
+          <p className="p-4">{description}</p>
         </div>
-        <p className="p-4">{description}</p>
-      </div>
-    </article>
+      </article>
+    </main>
   );
 }
---
+++ app/search/page.tsx
@@ -0,0 +1,15 @@
+import { NewsList } from '@/components';
+import { fetchNews } from '@/lib';
+
+type Props = {
+  searchParams?: { term: string };
+};
+export default async function Page({ searchParams }: Props) {
+  const news: NewsResponse = await fetchNews('general', searchParams?.term, true);
+  return (
+    <main>
+      <h1 className="headerTitle">Search: {searchParams?.term}</h1>
+      <NewsList news={news} />
+    </main>
+  );
+}
